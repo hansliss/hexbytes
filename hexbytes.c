f12b9faa1a867fbcc4c342a6887223d23652db19
hansliss <Hans@Liss.nu>
2017-11-14 13:22:15 +0100

    Added support for mixed format hex, skipping spaces and (limited) random non-hex characters. Added test target and a test case.

--- hexbytes.c
+++ hexbytes.c
@@ -3,15 +3,23 @@
 #include <unistd.h>
 #include <string.h>
 
+int isHexChar(char c) {
+  return strchr("0123456789abcdefABCDEF", c) != NULL;
+}
+
 int main(int argc, char *argv[]) {
   static char buf[8192];
   while (fgets(buf, 8192, stdin)) {
     int i=0;
-    while (i<strlen(buf)) {
+    int done=0;
+    while (!done) {
       unsigned int v;
+      while (i < strlen(buf) && (!isHexChar(buf[i]) || !strncasecmp(&(buf[i]), "0x", 2))) i++;
       if (sscanf(&(buf[i]), "%02X", &v) == 1)
 	fputc((char)v, stdout);
       i += 2;
+      if (i > strlen(buf)-2)
+	done=1;
     }
     //    fputc('\n', stdout);
   }
